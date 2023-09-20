13b7a74d1aa6632d72175829d7f6f7dcab1393d6
spmfte <spam.fete@gmail.com>
2023-09-20 16:16:17 -0700

    Improved modals to provide options for adding new assignments, reading materials, and notes.

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
-  "strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -19,11 +19,23 @@ const (
 	Timers
 )
 
+type ReadingMaterial struct {
+	Title  string
+	Author string
+	Read   bool
+}
+
 type model struct {
-	currentTab Tab
-	// Introducing state for assignments
-	assignments []string
+	currentTab         Tab
+	assignments        []string
 	selectedAssignment int
+	readingMaterials   []ReadingMaterial
+	selectedMaterial   int
+	notes              []string
+	selectedNote       int
+	showModal          bool
+	modalContent       string
+	// Incorporate timer and other states as we progress
 }
 
 var tabNames = []string{"Assignments", "Reading Materials", "Notes", "Timers"}
@@ -43,6 +55,14 @@ func initialModel() tea.Model {
 			"Book report on '1984'",
 			"Research on Romantic Era",
 		},
+		readingMaterials: []ReadingMaterial{
+			{Title: "Macbeth", Author: "Shakespeare", Read: true},
+			{Title: "1984", Author: "George Orwell", Read: false},
+		},
+		notes: []string{
+			"Note about Macbeth's main theme.",
+			"Personal thoughts on 1984.",
+		},
 	}
 }
 
@@ -53,6 +73,58 @@ func (m model) Init() tea.Cmd {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
+		if m.showModal {
+			if msg.String() == "esc" {
+				m.showModal = false
+			}
+			return m, nil
+		}
+		// Navigating and selections based on active tab
+		switch m.currentTab {
+		case Assignments:
+			switch msg.String() {
+			case "up":
+				if m.selectedAssignment > 0 {
+					m.selectedAssignment--
+				}
+			case "down":
+				if m.selectedAssignment < len(m.assignments)-1 {
+					m.selectedAssignment++
+				}
+			case "a":
+				m.showModal = true
+				m.modalContent = "Add new assignment"
+			}
+		case ReadingMaterials:
+			switch msg.String() {
+			case "up":
+				if m.selectedMaterial > 0 {
+					m.selectedMaterial--
+				}
+			case "down":
+				if m.selectedMaterial < len(m.readingMaterials)-1 {
+					m.selectedMaterial++
+				}
+			case "a":
+				m.showModal = true
+				m.modalContent = "Add new reading material"
+			}
+		case Notes:
+			switch msg.String() {
+			case "up":
+				if m.selectedNote > 0 {
+					m.selectedNote--
+				}
+			case "down":
+				if m.selectedNote < len(m.notes)-1 {
+					m.selectedNote++
+				}
+			case "a":
+				m.showModal = true
+				m.modalContent = "Add new note"
+			}
+		}
+		// Global navigation
 		switch msg.String() {
 		case "left":
 			if m.currentTab > 0 {
@@ -62,29 +134,22 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			if m.currentTab < Timers {
 				m.currentTab++
 			}
-		case "up":
-			if m.selectedAssignment > 0 {
-				m.selectedAssignment--
-			}
-		case "down":
-			if m.selectedAssignment < len(m.assignments)-1 {
-				m.selectedAssignment++
-			}
 		case "q":
 			return m, tea.Quit
 		}
 	case time.Time:
-		// Handle other interactions later
+		// Handle other interactions as needed
 	}
 	return m, nil
 }
 
 func (m model) View() string {
-	// Styles
+	// Styling
 	headerStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("#FFDD55")).Height(3).Align(lipgloss.Center)
 	tabStyle := lipgloss.NewStyle().Padding(1, 2).Foreground(lipgloss.Color("#888888"))
 	activeTabStyle := tabStyle.Copy().Foreground(lipgloss.Color("#FFDD55")).Underline(true)
 	selectedStyle := lipgloss.NewStyle().Background(lipgloss.Color("#FFDD55")).Foreground(lipgloss.Color("#000000"))
+	modalStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("#FFFFFF")).Background(lipgloss.Color("#333333")).Padding(2)
 
 	// Render tabs
 	var tabs []string
@@ -108,11 +173,33 @@ func (m model) View() string {
 			}
 		}
 	case ReadingMaterials:
-		content = "List of reading materials will be displayed here."
+		for i, material := range m.readingMaterials {
+			status := "Unread"
+			if material.Read {
+				status = "Read"
+			}
+			line := fmt.Sprintf("%s by %s (%s)", material.Title, material.Author, status)
+			if i == m.selectedMaterial {
+				content += selectedStyle.Render(line) + "\n"
+			} else {
+				content += line + "\n"
+			}
+		}
 	case Notes:
-		content = "Your notes will be displayed here."
+		for i, note := range m.notes {
+			if i == m.selectedNote {
+				content += selectedStyle.Render(note) + "\n"
+			} else {
+				content += note + "\n"
+			}
+		}
 	case Timers:
 		content = "Your timers will be displayed here."
+	}
+
+	if m.showModal {
+		modal := modalStyle.Render(m.modalContent)
+		content += "\n\n" + modal
 	}
 
 	// Combine the layout
