4c632a33b26786e049d258bfc13c1e4f2a19b15c
spmfte <spam.fete@gmail.com>
2023-09-20 16:42:41 -0700

    Added mouse interactions for list items and toggle read status for reading materials. Enhanced list item removal.

--- main.go
+++ main.go
@@ -43,6 +43,7 @@ type model struct {
 	showModal          bool
 	modalContent       string
 	inputValue         string
+	errorMessage       string
 }
 
 var tabNames = []string{"Assignments", "Reading Materials", "Notes", "Timers"}
@@ -89,28 +90,46 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			case "esc":
 				m.showModal = false
 				m.inputValue = ""
+				m.errorMessage = ""
 			case "enter":
+				m.errorMessage = ""
 				switch m.currentTab {
 				case Assignments:
-					m.assignments = append(m.assignments, m.inputValue)
+					if m.inputValue != "" {
+						m.assignments = append(m.assignments, m.inputValue)
+						m.showModal = false
+						m.inputValue = ""
+					}
 				case ReadingMaterials:
 					parts := strings.SplitN(m.inputValue, "-", 2)
 					if len(parts) == 2 {
 						m.readingMaterials = append(m.readingMaterials, ReadingMaterial{Title: strings.TrimSpace(parts[0]), Author: strings.TrimSpace(parts[1]), Read: false})
+						m.showModal = false
+						m.inputValue = ""
+					} else {
+						m.errorMessage = "Invalid format. Use 'Title - Author'"
 					}
 				case Notes:
-					m.notes = append(m.notes, m.inputValue)
+					if m.inputValue != "" {
+						m.notes = append(m.notes, m.inputValue)
+						m.showModal = false
+						m.inputValue = ""
+					}
 				case Timers:
 					parts := strings.SplitN(m.inputValue, "-", 2)
 					if len(parts) == 2 {
 						duration, err := time.ParseDuration(strings.TrimSpace(parts[1]))
 						if err == nil {
 							m.timers = append(m.timers, Timer{Description: strings.TrimSpace(parts[0]), Remaining: duration})
+							m.showModal = false
+							m.inputValue = ""
+						} else {
+							m.errorMessage = "Invalid duration format. Use '1h30m' etc."
 						}
+					} else {
+						m.errorMessage = "Invalid format. Use 'Description - Duration'"
 					}
 				}
-				m.showModal = false
-				m.inputValue = ""
 			default:
 				m.inputValue += msg.String()
 			}
@@ -132,6 +151,10 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			case "a":
 				m.showModal = true
 				m.modalContent = "Add new assignment:"
+			case "d":
+				if m.selectedAssignment >= 0 && m.selectedAssignment < len(m.assignments) {
+					m.assignments = append(m.assignments[:m.selectedAssignment], m.assignments[m.selectedAssignment+1:]...)
+				}
 			}
 		case ReadingMaterials:
 			switch msg.String() {
@@ -146,6 +169,14 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			case "a":
 				m.showModal = true
 				m.modalContent = "Add new reading material (Format: Title - Author):"
+			case "d":
+				if m.selectedMaterial >= 0 && m.selectedMaterial < len(m.readingMaterials) {
+					m.readingMaterials = append(m.readingMaterials[:m.selectedMaterial], m.readingMaterials[m.selectedMaterial+1:]...)
+				}
+			case "r":
+				if m.selectedMaterial >= 0 && m.selectedMaterial < len(m.readingMaterials) {
+					m.readingMaterials[m.selectedMaterial].Read = !m.readingMaterials[m.selectedMaterial].Read
+				}
 			}
 		case Notes:
 			switch msg.String() {
@@ -160,6 +191,10 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			case "a":
 				m.showModal = true
 				m.modalContent = "Add new note:"
+			case "d":
+				if m.selectedNote >= 0 && m.selectedNote < len(m.notes) {
+					m.notes = append(m.notes[:m.selectedNote], m.notes[m.selectedNote+1:]...)
+				}
 			}
 		case Timers:
 			switch msg.String() {
@@ -174,6 +209,10 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			case "a":
 				m.showModal = true
 				m.modalContent = "Set new timer (Format: Description - Duration):"
+			case "d":
+				if m.selectedTimer >= 0 && m.selectedTimer < len(m.timers) {
+					m.timers = append(m.timers[:m.selectedTimer], m.timers[m.selectedTimer+1:]...)
+				}
 			}
 		}
 		// Global navigation
@@ -214,6 +253,7 @@ func (m model) View() string {
 	activeTabStyle := tabStyle.Copy().Foreground(lipgloss.Color("#FFDD55")).Underline(true)
 	selectedStyle := lipgloss.NewStyle().Background(lipgloss.Color("#FFDD55")).Foreground(lipgloss.Color("#000000"))
 	modalStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("#FFFFFF")).Background(lipgloss.Color("#333333")).Padding(2)
+	errorStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("#FF0000"))
 
 	// Render tabs
 	var tabs []string
@@ -270,6 +310,9 @@ func (m model) View() string {
 
 	if m.showModal {
 		modalContent := m.modalContent + "\n" + m.inputValue + "|"
+		if m.errorMessage != "" {
+			modalContent += "\n" + errorStyle.Render(m.errorMessage)
+		}
 		modal := modalStyle.Render(modalContent)
 		content += "\n\n" + modal
 	}
